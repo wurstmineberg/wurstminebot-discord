0fd58bb4cf049ed2b5e2c49431889d5f00098953
Fenhl <fenhl@fenhl.net>
2020-09-12 05:52:50 +0000

    Add Python frontend for IPC commands

---
+++ crate/wurstminebot-python/src/lib.rs
@@ -0,0 +1,61 @@
+#![deny(rust_2018_idioms, unused, unused_import_braces, unused_qualifications, warnings)]
+
+use {
+    pyo3::{
+        create_exception_type_object,
+        impl_exception_boilerplate,
+        prelude::*,
+        wrap_pyfunction
+    },
+    serenity::{
+        model::prelude::*,
+        utils::MessageBuilder
+    }
+};
+
+struct CommandError;
+
+impl_exception_boilerplate!(CommandError);
+
+fn user_to_id(user: &PyAny) -> PyResult<UserId> {
+    if let Ok(snowflake) = user.getattr("snowflake") {
+        // support wurstmineberg_web.models.Person arguments
+        Ok(UserId(snowflake.extract()?))
+    } else if let Ok(wmbid) = user.getattr("wmbid") {
+        Err(CommandError::py_err(format!("Wurstmineberg member {} has no Discord snowflake", wmbid)))
+    } else {
+        // support plain snowflakes
+        Ok(UserId(user.extract()?))
+    }
+}
+
+#[pyfunction] fn escape(text: &str) -> String {
+    let mut builder = MessageBuilder::default();
+    builder.push_safe(text);
+    builder.build()
+}
+
+#[pyfunction] fn channel_msg(channel_id: u64, msg: String) -> PyResult<()> {
+    wurstminebot_ipc::channel_msg(ChannelId(channel_id), msg)
+        .map_err(|e| CommandError::py_err(e.to_string()))
+}
+
+#[pyfunction] fn quit() -> PyResult<()> {
+    wurstminebot_ipc::quit()
+        .map_err(|e| CommandError::py_err(e.to_string()))
+}
+
+#[pyfunction] fn set_display_name(user_id: &PyAny, new_display_name: String) -> PyResult<()> {
+    wurstminebot_ipc::set_display_name(user_to_id(user_id)?, new_display_name)
+        .map_err(|e| CommandError::py_err(e.to_string()))
+}
+
+#[pymodule] fn wurstminebot(_: Python<'_>, m: &PyModule) -> PyResult<()> {
+    create_exception_type_object!(m, CommandError, pyo3::exceptions::RuntimeError);
+    m.add_wrapped(wrap_pyfunction!(escape))?;
+    //TODO make sure that all IPC commands are listed below
+    m.add_wrapped(wrap_pyfunction!(channel_msg))?;
+    m.add_wrapped(wrap_pyfunction!(quit))?;
+    m.add_wrapped(wrap_pyfunction!(set_display_name))?;
+    Ok(())
+}
